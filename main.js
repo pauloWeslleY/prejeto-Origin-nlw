ad6fcba0428f23215f5bb0717f598d9166dba2ac
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-01-16 17:16:43 -0300

    nlw aula 2 menu animado

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ for (const link of links) {
       nav.classList.remove('show')
    })
 }
+
+// NOTE: Coloca sombra no header quando der scroll
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+   if (window.scrollY >= navHeight) {
+      // maior que a altura do header
+      header.classList.add('scroll')
+   } else {
+      // menor que a altura do header
+      header.classList.remove('scroll')
+   }
+})
