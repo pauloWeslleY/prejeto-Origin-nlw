338670e1c40e075826650151f9f58bacae1e9972
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-01-16 15:10:18 -0300

    nlw aula 2 menu-mobile finalizado

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 // DOM -> Document Object Model
+// NOTE: abre e fecha o menu quando clicar no icone
 const nav = document.querySelector('#header nav')
 const toogle = document.querySelectorAll("nav .toogle")
 
 for (const element of toogle) {
    element.addEventListener('click', function () {
       nav.classList.toggle('show')
+   })
+}
+
+// NOTE: quando clicar no item do menu, esconder o menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+   link.addEventListener('click', function () {
+      nav.classList.remove('show')
    })
 }
