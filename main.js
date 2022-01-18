9a9f638ac1a37a050be66c700a34b267aa9a5c08
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-01-18 14:03:44 -0300

    nlw-06-origin secetion testimonials finalizada

--- main.js
+++ main.js
@@ -31,3 +31,13 @@ window.addEventListener('scroll', function () {
       header.classList.remove('scroll')
    }
 })
+
+/* HACK: Pluign Testimonials Slider -> Carousel */
+const swiper = new Swiper('.swiper', {
+   slidesPerView: 1,
+   pagination: {
+      el: '.swiper-pagination'
+   },
+   mousewheel: true,
+   keyboard: true
+})
